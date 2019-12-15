40923eefcad10b41bbf6de9bc591d354d86c4d79
Munaiz <munaizahmed2@gmail.com>
2019-12-15 12:50:15 -0600

    working on singlePlant view

--- src/Components/SinglePlant.js
+++ src/Components/SinglePlant.js
@@ -1,16 +1,27 @@
 import React from "react";
+import plants from "../Plants.json";
 
-class SinglePlant extends React.Component{
-
-
-  render(){
-    console.log('URL?',this.props.match.params)
-    return(
-      <div>
-        Single Plant
+class SinglePlant extends React.Component {
+  render() {
+    let currentPlantObj = plants
+      .filter(plant => plant.name === this.props.match.params.plantName)
+      .pop();
+    console.log(currentPlantObj);
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center"
+        }}
+      >
+        <h1>{currentPlantObj.name}</h1>
+        <div>
+          <h6>Water every {currentPlantObj.water_after}</h6>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default SinglePlant
+export default SinglePlant;
