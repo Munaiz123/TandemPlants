954b692ac33f42a8fb5ebe9abdd3dd66f10d5784
Munaiz <munaizahmed2@gmail.com>
2019-12-16 15:55:51 -0600

    backing up to github

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,8 +3,9 @@ import React from "react";
 export default class Nav extends React.Component {
   render() {
     return (
-      <div style={{display:'flex', justifyContent:'center'}}>
-
+      <div style={{display:'flex', flexDirection:'column', alignItems:'center'}}>
+        <h1>Welcome to Tandem Plants</h1>
+        <h3>Munaiz Ahmed</h3>
       </div>
     );
   }
--- src/Components/Plants.spec.js
+++
@@ -1,8 +0,0 @@
-const {expect} = require('chai')
-
-
-
-describe( 'Tandem Plants', () => {
-  it( 'Write Tests', ()=>{
-  })
-})
